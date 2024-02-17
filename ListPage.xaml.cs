56861af5bb36d2cca2513ab6a795eea9b77bfeae
RusMihaiRobert <feroromid@yahoo.com>
2024-02-18 00:07:02 +0200

    Labb8

---
+++ ListPage.xaml.cs
@@ -0,0 +1,28 @@
+using Rus_MihaiRobert_Lab7.Models;
+
+namespace Rus_MihaiRobert_Lab7;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+
+
+}
